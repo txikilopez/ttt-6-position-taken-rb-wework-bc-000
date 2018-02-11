f60ff40c68675db9eab806f652dd27df3404c152
Antonio Troncoso <antonio.troncoso@wework.com>
2018-02-11 23:07:18 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+
+def position_taken?(board1, index1)
+  if board1[index1] ==" " || board1[index1] =="" || board1[index1] == nil
+    false
+  elsif board1[index1] == "X" || board1[index1] == "O"
+  true
+  end
+end
